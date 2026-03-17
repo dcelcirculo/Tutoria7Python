6a706f94cb39d49bb71f75565c7bc945dea226d1
CaroGil <dcgil83@gmail.com>
2026-03-17 16:58:13 -0700

    Adding ejercicio9

---
+++ ejercicio9.py
@@ -0,0 +1,6 @@
+#9- Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las letras de la palabra introducida empezando por la última.
+
+word = input("Digite una palabra: ")
+
+for letter in word:
+    print(letter)
