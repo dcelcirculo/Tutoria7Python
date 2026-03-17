a24e06f89cdc87b8686e3c0b18a9eb1bea2f3048
CaroGil <dcgil83@gmail.com>
2026-03-17 16:39:41 -0700

    Se agrega ejercicio 6

---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+#1- Escribir un programa que pregunte al usuario su edad y muestre  por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+
+age = int(input("Digite su edad: "))
+anio_actual = 2026
+
+for y in range(0, age+1):
+    print(anio_actual)
+    anio_actual -= 1
